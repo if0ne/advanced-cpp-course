004e1b39908f23b3329a6956650f62a77201f7dc
Pavel Agafonov <pavel.agafonov.al@gmail.com>
2023-10-14 16:41:28 +0300

    Modified quick sort

--- AdvancedCppCourse/QuickSort/Quicksort.h
+++ AdvancedCppCourse/QuickSort/Quicksort.h
@@ -2,10 +2,10 @@
 #include <utility>
 
 template<typename T>
-void swap(T* left, T* right) {
-    auto temp = std::move(left);
-    new (left) T(std::move(right));
-    new (right) T(std::move(&temp));
+void swap(T& left, T& right) {
+    T temp(std::move(left));
+    left = std::move(right);
+    right = std::move(temp);
 }
 
 template<typename T, typename Compare>
@@ -15,15 +15,15 @@ T* select_pivot(T* first, T* last, Compare comp) {
     T& middle_pivot = *(first + (last - first) / 2);
 
     if (comp(first_pivot, middle_pivot)) {
-        swap(&first_pivot, &middle_pivot);
+        swap(first_pivot, middle_pivot);
     }
 
     if (comp(middle_pivot, last_pivot)) {
-        swap(&middle_pivot, &last_pivot);
+        swap(middle_pivot, last_pivot);
     }
 
     if (comp(first_pivot, middle_pivot)) {
-        swap(&first_pivot, &middle_pivot);
+        swap(first_pivot, middle_pivot);
     }
 
     return &middle_pivot;
@@ -36,7 +36,7 @@ void partition(T* first, T* last, T* pivot, Compare comp) {
         while (comp(*pivot, *last)) last--;
 
         if (first <= last) {
-            swap(first, last);
+            swap(*first, *last);
 
             if (first == pivot) {
                 pivot = last;
@@ -53,6 +53,60 @@ void partition(T* first, T* last, T* pivot, Compare comp) {
 }
 
 template<typename T, typename Compare>
+void quick_sort_modified(T* first, T* last, Compare comp) {
+    const int INSERTION_SORT_THRESHOLD = 64;
+    if (first >= last) {
+        return;
+    }
+
+    if (last - first < INSERTION_SORT_THRESHOLD) {
+        insertion_sort(first, last, comp);
+        return;
+    }
+
+    T* pivot = select_pivot(first, last, comp);
+    partition(first, last, pivot, comp);
+    
+    if (pivot - first < last - pivot) {
+        quick_sort_iterative(pivot + 1, last, comp);
+        quick_sort(first, pivot - 1, comp);
+    } else {
+        quick_sort_iterative(first, pivot - 1, comp);
+        quick_sort(pivot + 1, last, comp);
+    }
+}
+
+template<typename T, typename Compare>
+void quick_sort_iterative(T* first, T* last, Compare comp) {
+    T** stack = new T*[last - first + 1];
+
+    int top = 0;
+
+    stack[top++] = first;
+    stack[top++] = last;
+
+    while (top > 0) {
+        T* l = stack[--top];
+        T* f = stack[--top];
+
+        T* p = select_pivot(f, l, comp);
+        partition(f, l, p, comp);
+
+        if (p - 1 > f) {
+            stack[top++] = f;
+            stack[top++] = p - 1;
+        }
+
+        if (p + 1 < l) {
+            stack[top++] = p + 1;
+            stack[top++] = l;
+        }
+    }
+
+    delete[] stack;
+}
+
+template<typename T, typename Compare>
 void quick_sort(T* first, T* last, Compare comp) {
     if (first >= last) {
         return;
@@ -60,12 +114,18 @@ void quick_sort(T* first, T* last, Compare comp) {
 
     T* pivot = select_pivot(first, last, comp);
     partition(first, last, pivot, comp);
-    
-    quick_sort(first, pivot, comp);
+
+    quick_sort(first, pivot - 1, comp);
     quick_sort(pivot + 1, last, comp);
 }
 
 template<typename T, typename Compare>
 void insertion_sort(T* first, T* last, Compare comp) {
-    
+    for (T* i = first + 1; i < last; i++) {
+        T* j = i;
+        while (j > first && comp(*j, *(j - 1))) {
+            swap(*j, *(j - 1));
+            j--;
+        }
+    }
 }
--- AdvancedCppCourse/QuickSort/main.cpp
+++ AdvancedCppCourse/QuickSort/main.cpp
@@ -1,7 +1,39 @@
 #include "Quicksort.h"
+#include <chrono>
+#include <iostream>
+#include <vector>
+
+template <
+    class result_t = std::chrono::microseconds,
+    class clock_t = std::chrono::steady_clock,
+    class duration_t = std::chrono::milliseconds
+>
+auto since(std::chrono::time_point<clock_t, duration_t> const& start)
+{
+    return std::chrono::duration_cast<result_t>(clock_t::now() - start);
+}
+
+std::vector<int> generate(int n) {
+    std::vector<int> foo;
+
+    for (int i = 0; i < n; i++) {
+        foo.push_back(n - i);
+    }
+
+    return foo;
+}
 
 int main() {
-    int a[] = { 5, 4, 6, 7, 3, 2, 1};
+    auto a = generate(10000);
+    auto b = generate(10000);
 
-    quick_sort(a, a + 6, [](int a, int b) { return a < b; });
+    auto start = std::chrono::steady_clock::now();
+    quick_sort(a.data(), a.data() + a.size() - 1, [](int a, int b) { return a < b; });
+    std::cout << "Elapsed(us)=" << since(start).count() << std::endl;
+
+    start = std::chrono::steady_clock::now();
+    quick_sort_modified(b.data(), b.data() + b.size() - 1, [](int a, int b) { return a < b; });
+    std::cout << "Elapsed(us)=" << since(start).count() << std::endl;
+
+    int c = 0;
 }
